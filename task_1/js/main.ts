7ba518e22b762688618f313dc5bd5b729a1792b4
FYUDerma <a.zakaria.pro@gmail.com>
2025-01-29 14:20:01 +0100

    Refactor Teacher interface to use readonly properties and add StudentClass implementation with methods for homework and display name

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,34 +1,15 @@
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
+  [propName: string]: any;
 }
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
 
 interface Directors extends Teacher {
   numberOfReports: number;
 }
-
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
@@ -38,4 +19,37 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-console.log(printTeacher('John', 'Doe'));
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+      return 'Currently working';
+  }
+
+  displayName() {
+    return `${this.firstName}`;
+  }
+}
+
+export {
+  printTeacher,
+  StudentClass
+}
+
+const obj:StudentClassInterface = new StudentClass('John', 'Doe');
+console.log(obj);
