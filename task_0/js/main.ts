6e40ceaa580cbc0576a688c268e29ca9ee3dbe9e
FYUDerma <a.zakaria.pro@gmail.com>
2025-01-29 10:44:50 +0100

    Update student list display and upgrade TypeScript version

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -19,4 +19,32 @@ const student2: Student = {
   location: 'California'
 }
 
-const studentslist: Array<Student> = [student1, student2];
+const studentsList: Array<Student> = [student1, student2];
+const table = document.createElement('table');
+const headerRow = document.createElement('tr');
+
+const firstNameHeader = document.createElement('th');
+firstNameHeader.textContent = 'First Name';
+headerRow.appendChild(firstNameHeader);
+
+const locationHeader = document.createElement('th');
+locationHeader.textContent = 'Location';
+headerRow.appendChild(locationHeader);
+
+table.appendChild(headerRow);
+
+studentsList.forEach(student => {
+  const row = document.createElement('tr');
+
+  const firstName = document.createElement('td');
+  firstName.textContent = student.firstName;
+  row.appendChild(firstName);
+
+  const location = document.createElement('td');
+  location.textContent = student.location;
+  row.appendChild(location);
+
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
